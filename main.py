7a71a55aa8310b4ac80e69825448172522999be1
bryansans <150808504+bryansans@users.noreply.github.com>
2023-12-02 16:32:33 +0000

    https://github.com/bryansans/Inventario.git

--- main.py
+++ main.py
@@ -7,18 +7,73 @@ from datetime import datetime
 
 def reconocer_voz():
     # TODO: Implementar la lógica para reconocer voz
+    recognizer = sr.Recognizer()
+    mic = sr.Microphone()
+    
+    with mic as source:
+    print("Habla ahora...")
+    Recognizer.adjust_for_ambient_noise(source)
+    audio = Recognizer.listen(source)
+
+    try:
+    print("Reconociendo...")
+    texto = recognizer.recognize_google(audio, language='es-ES')
+    print(f"Texto reconocido: {texto}")
+    return texto
+    except sr.UnknownValueError:
+    print("No se pudo entender el audio")
+    return ""
+    except sr.RequestError:
+    print("Error en la solicitud")
+    return ""
 
 
 def exportar_a_excel(inventario):
     # TODO: Implementar la lógica para exportar a Excel
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.title = "Inventario"
+
+    sheet['A1'] = 'Producto'
+    sheet['B1'] = 'Cantidad'
+
+    row = 2
+    for producto, cantidad in inventario ¿.items():
+    sheet.cell(row=row, column=1).value = producto
+    sheet.cell(row=row, column=2).value = cantidad
+    row += 1
+
+    wb.save('inventario.xlsx')
+    print("Inventario exportado a Excel exitosamente.")
 
 
 def exportar_a_pdf(inventario):
     # TODO: Implementar la lógica para exportar a PDF
+    pdf = canvas.Canvas("inventario.pdf")
+    pdf.drawString(100, 800, "Inventario")
+
+    y = 750
+    for producto, cantidad in inventario.items():
+    pdf.drawString(100, y, f"Producto: {producto}, Cantidad: {cantidad}")
+    y -= 20
+
+    pdf.save()
+    print("Inventario exportado a PDF exitosamente.")
 
 
 def mostrar_grafico_salidas(inventario):
     # TODO: Implementar la lógica para mostrar gráfico de salidas
+    productos = list(inventario.keys())
+    cantidades = list(inventario.values())
+
+    plt.bar(productos, cantidades)
+    plt.xlabel('Productos')
+    plt.ylabel('Cantidad')
+    plt.title('Inventario de Productos')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    plt.show()
 
 
 def main():
